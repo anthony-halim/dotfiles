128ec75374b63375b05d27a1e4a4822633d8be43
Anthony Halim <50617144+anthony-halim@users.noreply.github.com>
2023-07-31 15:46:19 +0800

    fix(setup): setup script (#4)

--- scripts/bash_template.sh
+++ scripts/bash_template.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 set -Eeuo pipefail
-trap cleanup SIGINT SIGTERM ERR EXIT
+trap catch_err ERR
+trap cleanup SIGINT SIGTERM EXIT
 
-script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 usage() {
-	cat <<EOF # remove the space between << and EOF, this is due to web plugin issue
+  cat <<EOF # remove the space between << and EOF, this is due to web plugin issue
 Usage: $(
-		basename "${BASH_SOURCE[0]}"
-	) [-h] [-v] [-f] -p param_value arg1 [arg2...]
+    basename "${BASH_SOURCE[0]}"
+  ) [-h] [-v] [-f] -p param_value arg1 [arg2...]
 
 Script description here.
 
@@ -20,61 +21,93 @@ Available options:
 -f, --flag      Some flag description
 -p, --param     Some param description
 EOF
-	exit
+  exit
 }
 
 cleanup() {
-	trap - SIGINT SIGTERM ERR EXIT
-	# script cleanup here
+  trap - SIGINT SIGTERM EXIT
+  # script cleanup here
+}
+
+catch_err() {
+  msg_err "$(caller): Execution failed at this line."
+  cleanup
 }
 
 setup_colors() {
-	if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-		NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
-	else
-		NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
-	fi
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+  else
+    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
+
+separator() {
+  msg "${CYAN}-------------------------------------${NOFORMAT}"
 }
 
 msg() {
-	echo >&2 -e "${1-}"
+  echo >&2 -e "${1-}"
+}
+
+msg_info() {
+  msg "${GREEN}$1${NOFORMAT}"
+}
+
+msg_warn() {
+  msg "${ORANGE}$1${NOFORMAT}"
+}
+
+msg_err() {
+  msg "${RED}$1${NOFORMAT}"
 }
 
 die() {
-	local msg=$1
-	local code=${2-1} # default :exit status 1
-	msg "$msg"
-	exit "$code"
+  local msg=$1
+  local code=${2-1} # default :exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
+confirm() {
+  while true; do
+    read -r -p "  Do you want to proceed? [Y]es/[N]o) " yn
+    case $yn in
+    [Yy]*) return 0 ;;
+    [Nn]*) return 1 ;;
+    *) msg "  Please answer [Y]es or [N]o." ;;
+    esac
+  done
 }
 
 parse_params() {
-	# default values of variables set from params
-	flag=0
-	param=''
+  # default values of variables set from params
+  flag=0
+  param=''
 
-	while :; do
-		case "${1-}" in
-		-h | --help) usage ;;
-		-v | --verbose) set -x ;;
-		--no-color) NO_COLOR=1 ;;
-		-f | --flag) flag=1 ;; # example flag
-		-p | --param)          # example named parameter
-			param="${2-}"
-			shift
-			;;
-		-?*) die "Unknown option: $1" ;;
-		*) break ;;
-		esac
-		shift
-	done
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -v | --verbose) set -x ;;
+    --no-color) NO_COLOR=1 ;;
+    -f | --flag) flag=1 ;; # example flag
+    -p | --param)          # example named parameter
+      param="${2-}"
+      shift
+      ;;
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
 
-	args=("$@")
+  args=("$@")
 
-	# check required params and arguments
-	[[ -z "${param-}" ]] && die "Missing required parameter: param"
-	[[ ${#args[@]} -eq 0 ]] && die "Missing script arguments"
+  # check required params and arguments
+  [[ -z "${param-}" ]] && die "Missing required parameter: param"
+  [[ ${#args[@]} -eq 0 ]] && die "Missing script arguments"
 
-	return 0
+  return 0
 }
 
 parse_params "$@"
