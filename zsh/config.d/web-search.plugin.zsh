dd6528b1fbbad19ac9001f6ea17a5198af62d4e4
Anthony Halim <anthonyhalim1997@gmail.com>
2023-08-28 21:19:00 +0800

    refactor sudo and web-search out of omz

---
+++ zsh/config.d/web-search.plugin.zsh
@@ -0,0 +1,68 @@
+# Taken from: https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/web-search
+# Credit to the original authors.
+# ------------------------------------------------------------------------------
+# Description
+# -----------
+#
+# web_search from terminal.
+#
+# ------------------------------------------------------------------------------
+
+function web_search() {
+  emulate -L zsh
+
+  # define search engine URLS
+  typeset -A urls
+  urls=(
+    $ZSH_WEB_SEARCH_ENGINES
+    google          "https://www.google.com/search?q="
+    duckduckgo      "https://www.duckduckgo.com/?q="
+    github          "https://github.com/search?q="
+    stackoverflow   "https://stackoverflow.com/search?q="
+    wolframalpha    "https://www.wolframalpha.com/input/?i="
+  )
+
+  # check whether the search engine is supported
+  if [[ -z "$urls[$1]" ]]; then
+    echo "Search engine '$1' not supported."
+    return 1
+  fi
+
+  # search or go to main page depending on number of arguments passed
+  if [[ $# -gt 1 ]]; then
+    # build search url:
+    # join arguments passed with '+', then append to search engine URL
+    url="${urls[$1]}$(omz_urlencode ${@[2,-1]})"
+  else
+    # build main page url:
+    # split by '/', then rejoin protocol (1) and domain (2) parts with '//'
+    url="${(j://:)${(s:/:)urls[$1]}[1,2]}"
+  fi
+
+  open_command "$url"
+}
+
+
+alias google='web_search google'
+alias ddg='web_search duckduckgo'
+alias github='web_search github'
+alias stackoverflow='web_search stackoverflow'
+alias wolframalpha='web_search wolframalpha'
+
+#add your own !bang searches here
+alias wiki='web_search duckduckgo \!w'
+alias news='web_search duckduckgo \!n'
+alias youtube='web_search duckduckgo \!yt'
+alias map='web_search duckduckgo \!m'
+alias image='web_search duckduckgo \!i'
+alias ducky='web_search duckduckgo \!'
+
+# other search engine aliases
+if [[ ${#ZSH_WEB_SEARCH_ENGINES} -gt 0 ]]; then
+  typeset -A engines
+  engines=($ZSH_WEB_SEARCH_ENGINES)
+  for key in ${(k)engines}; do
+    alias "$key"="web_search $key"
+  done
+  unset engines key
+fi
