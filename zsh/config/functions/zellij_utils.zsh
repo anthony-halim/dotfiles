4534e371a3f25e191966fe4d35031a6dfdae0c03
Anthony Halim <anthonyhalim1997@gmail.com>
2023-11-01 17:01:17 +0800

    fix(zellij): fallback tab name to directory

--- zsh/config/functions/zellij_utils.zsh
+++ zsh/config/functions/zellij_utils.zsh
@@ -5,9 +5,16 @@ zellij_tab_name_update_by_git_repo() {
     if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
         tab_name+=$(basename -s .git "$(git config --get remote.origin.url)")/
         tab_name=${tab_name%/}
-
-        command nohup zellij action rename-tab "$tab_name" >/dev/null 2>&1
+    else
+        tab_name=$PWD
+        if [[ $tab_name == $HOME ]]; then
+         	tab_name="~"
+        else
+         	tab_name=${tab_name##*/}
+        fi
     fi
+
+    command nohup zellij action rename-tab "$tab_name" >/dev/null 2>&1
   fi
 }
 
