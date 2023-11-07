1c9d95b6a489767373f53d402e3069539541f863
Anthony Halim <anthonyhalim1997@gmail.com>
2023-11-08 00:09:01 +0800

    fix(lualine): revert removing filename from lualine

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -33,8 +33,10 @@ return {
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch" },
-        lualine_c = { "encoding", "fileformat", "filetype" },
-        lualine_x = {},
+        lualine_c = {
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+        },
         lualine_y = {
           { "progress", separator = " ",                  padding = { left = 1, right = 0 } },
           { "location", padding = { left = 0, right = 1 } },
