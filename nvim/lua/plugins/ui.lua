0820a0ce54920322bbeee477a28aaaeb64cbf9a4
Anthony Halim <anthonyhalim1997@gmail.com>
2023-11-17 19:26:46 +0800

    revert nvim-notify

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,16 +1,34 @@
 return {
+  -- LSP progress
   {
     "j-hui/fidget.nvim",
     opts = {
       notification = {
-        override_vim_notify = true,
+        override_vim_notify = false,
         window = {
-          winblend = 0,
-          border = "rounded",
           y_padding = 1,
         },
       },
     },
+  },
+
+  -- Better `vim.notify()`
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+      stages = "fade_in_slide_out",
+    },
+    config = function(_, opts)
+      require("notify").setup(opts)
+      vim.notify = require("notify")
+    end,
   },
 
   -- statusline
