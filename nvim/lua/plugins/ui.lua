d89334a23060416e6d88064693d6a0bae069eb09
Anthony Halim <anthonyhalim1997@gmail.com>
2023-11-17 11:04:25 +0800

    fix(nvim): remove time from lualine

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -32,15 +32,6 @@ return {
           { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
           { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
         },
-        lualine_y = {
-          { "progress", separator = " ",                  padding = { left = 1, right = 0 } },
-          { "location", padding = { left = 0, right = 1 } },
-        },
-        lualine_z = {
-          function()
-            return " " .. os.date("%R")
-          end,
-        },
       },
       extensions = { "nvim-tree", "lazy" },
     },
