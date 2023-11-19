6fdc49b40e922e2828f1b7d89fda76c3db576b9a
Anthony Halim <anthonyhalim1997@gmail.com>
2023-11-19 20:21:45 +0800

    fix(nvim): remove indent-blankline and venv-selector

--- nvim/lua/plugins/lang/python.lua
+++ nvim/lua/plugins/lang/python.lua
@@ -22,17 +22,4 @@ return {
       },
     },
   },
-  {
-    "linux-cultist/venv-selector.nvim",
-    cmd = "VenvSelect",
-    opts = {
-      name = {
-        "venv",
-        ".venv",
-        "env",
-        ".env",
-      },
-    },
-    keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
-  },
 }
