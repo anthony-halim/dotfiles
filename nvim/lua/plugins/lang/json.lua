cdb097d1e24bb27c3d65511a9ff075148271d876
Anthony Halim <anthonyhalim1997@gmail.com>
2024-02-14 21:35:15 +0800

    fix(nvim): remove SchemaStore

--- nvim/lua/plugins/lang/json.lua
+++ nvim/lua/plugins/lang/json.lua
@@ -9,20 +9,10 @@ return {
     end,
   },
   {
-    "b0o/SchemaStore.nvim",
-    lazy = true,
-  },
-  {
     "neovim/nvim-lspconfig",
     opts = {
-      -- make sure mason installs the server
       servers = {
         jsonls = {
-          -- lazy-load schemastore when needed
-          on_new_config = function(new_config)
-            new_config.settings.json.schemas = new_config.settings.json.schemas or {}
-            vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
-          end,
           settings = {
             json = {
               format = {
--- nvim/lua/plugins/lang/yaml.lua
+++ nvim/lua/plugins/lang/yaml.lua
@@ -9,9 +9,6 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      "b0o/SchemaStore.nvim",
-    },
     opts = {
       servers = {
         yamlls = {
@@ -24,14 +21,6 @@ return {
               },
             },
           },
-          -- -- lazy-load schemastore when needed
-          -- on_new_config = function(new_config)
-          --   new_config.settings.yaml.schemas = vim.tbl_deep_extend(
-          --     "force",
-          --     new_config.settings.yaml.schemas or {},
-          --     require("schemastore").yaml.schemas()
-          --   )
-          -- end,
           settings = {
             redhat = { telemetry = { enabled = false } },
             yaml = {
@@ -40,13 +29,6 @@ return {
                 enable = true,
               },
               validate = true,
-              schemaStore = {
-                -- Must disable built-in schemaStore support to use
-                -- schemas from SchemaStore.nvim plugin
-                enable = false,
-                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-                url = "",
-              },
             },
           },
         },
