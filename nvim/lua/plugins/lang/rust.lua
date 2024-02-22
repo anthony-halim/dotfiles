1b77f13ca487c979a1ea3bfa89ee89cdadad9a60
Anthony Halim <anthonyhalim1997@gmail.com>
2024-02-22 17:43:29 +0800

    fix(starship): add timestamp of cmd execution

--- nvim/lua/plugins/lang/rust.lua
+++ nvim/lua/plugins/lang/rust.lua
@@ -58,21 +58,21 @@ return {
             },
           },
         },
-        taplo = {
-          keys = {
-            {
-              "K",
-              function()
-                if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
-                  require("crates").show_popup()
-                else
-                  vim.lsp.buf.hover()
-                end
-              end,
-              desc = "Show Crate Documentation",
-            },
-          },
-        },
+        -- taplo = {
+        --   keys = {
+        --     {
+        --       "K",
+        --       function()
+        --         if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+        --           require("crates").show_popup()
+        --         else
+        --           vim.lsp.buf.hover()
+        --         end
+        --       end,
+        --       desc = "Show Crate Documentation",
+        --     },
+        --   },
+        -- },
       },
     },
   },
