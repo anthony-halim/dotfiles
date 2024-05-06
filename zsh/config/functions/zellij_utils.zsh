2c6847b94196b9fdf041a85792d1cee30aaf5a73
Anthony Halim <anthonyhalim1997@gmail.com>
2024-05-06 20:59:41 +0800

    fix(zellij): default pane to be under default_tab_template

--- zsh/config/functions/zellij_utils.zsh
+++ zsh/config/functions/zellij_utils.zsh
@@ -1,32 +1,31 @@
 # Auto rename zellij tab if in Git repository
 zellij_tab_name_update_by_git_repo() {
-  if [[ -n $ZELLIJ ]]; then
-    tab_name=''
-    if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-        tab_name+=$(basename -s .git "$(git config --get remote.origin.url)")/
-        tab_name="git - ${tab_name%/}"
-    else
-        tab_name=$PWD
-        if [[ $tab_name == $HOME ]]; then
-         	tab_name="~"
-        else
-         	tab_name=${tab_name##*/}
-        fi
-    fi
-
-    command nohup zellij action rename-tab "$tab_name" >/dev/null 2>&1
-  fi
+	if [[ -n $ZELLIJ ]]; then
+		tab_name=''
+		if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+			tab_name+=$(basename "$(git rev-parse --show-toplevel)")
+			tab_name="git - ${tab_name%/}"
+		else
+			tab_name=$PWD
+			if [[ $tab_name == $HOME ]]; then
+				tab_name="~"
+			else
+				tab_name=${tab_name##*/}
+			fi
+		fi
+		command nohup zellij action rename-tab "$tab_name" >/dev/null 2>&1
+	fi
 }
 
 zellij_autostart() {
-  if [[ $(command -v zellij) && "$ZELLIJ_AUTO_START" = true ]]; then
-    # From 'eval "$(zellij setup --generate-auto-start zsh)"'
-    if [[ -z "$ZELLIJ" ]]; then
-        if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
-            zellij attach -c
-        else
-            zellij 
-        fi
-    fi
-  fi
+	if [[ $(command -v zellij) && "$ZELLIJ_AUTO_START" = true ]]; then
+		# From 'eval "$(zellij setup --generate-auto-start zsh)"'
+		if [[ -z "$ZELLIJ" ]]; then
+			if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
+				zellij attach -c
+			else
+				zellij
+			fi
+		fi
+	fi
 }
