9fc2220572b86cdae3ff001069b7ada0fe577c2d
Anthony Halim <anthonyhalim1997@gmail.com>
2024-12-08 16:05:30 +0800

    feat(nvim)!: replace nvim-cmp with blink.cmp (#31)

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -16,84 +16,28 @@ return {
 
   -- Autocompletion
   {
-    "hrsh7th/nvim-cmp",
-    version = false, -- last release is way too old
-    event = "InsertEnter",
-    dependencies = {
-      -- Adds LSP completion capabilities
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-
-      -- Adds a number of user-friendly snippets
-      "rafamadriz/friendly-snippets",
-
-      -- Snippet Engine & its associated nvim-cmp source
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
+    "saghen/blink.cmp",
+    lazy = false, -- lazy loading handled internally
+    -- use a release tag to download pre-built binaries
+    version = "v0.*",
+    opts = {
+      keymap = {
+        preset = "enter",
+      },
+      completion = {
+        documentation = {
+          auto_show = true,
+        },
+        menu = {
+          draw = {
+            columns = {
+              { "kind_icon", "kind",              gap = 1 },
+              { "label",     "label_description", gap = 1 },
+            },
+          }
+        },
+      },
     },
-    opts = function()
-      local cmp = require("cmp")
-      local compare = require("cmp.config.compare")
-      local luasnip = require("luasnip")
-
-      return {
-        completion = {
-          completeopt = "menu,menuone,noinsert",
-        },
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
-          }),
-          ["<S-CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "path" },
-        }, {
-          { name = "buffer" },
-        }),
-        formatting = {
-          format = function(_, item)
-            local icons = require("config").options.icons.kinds
-            if icons[item.kind] then
-              item.kind = icons[item.kind] .. item.kind
-            end
-            return item
-          end,
-        },
-        sorting = {
-          comparators = {
-            compare.exact,
-            compare.recently_used,
-            compare.locality,
-            compare.offset,
-            -- compare.scopes,
-            compare.score,
-            compare.kind,
-            -- compare.sort_text,
-            -- compare.length,
-            -- compare.order,
-          },
-        },
-      }
-    end,
   },
 
   -- auto pairs
