2993a17fd455fa9e243979c40c1222a3ad21da20
Anthony Halim <anthonyhalim1997@gmail.com>
2025-01-03 11:12:28 +0800

    fix(nvim): replace mini.indentscope with snacks.nvim indent

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -10,6 +10,7 @@ return {
         -- Input at cursor position
         input = { relative = "cursor", row = -3, col = 0 },
       },
+      indent = { enabled = true },
     },
     init = function()
       -- LSP Progress via notification
@@ -134,39 +135,6 @@ return {
       end,
     },
     config = true,
-  },
-
-  -- Active indent guide and indent text objects. When you're browsing
-  -- code, this highlights the current level of indentation, and animates
-  -- the highlighting.
-  {
-    "echasnovski/mini.indentscope",
-    version = false, -- wait till new 0.7.0 release to put it back on semver
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-      symbol = "│",
-      options = { try_as_border = true },
-    },
-    init = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = {
-          "help",
-          "alpha",
-          "dashboard",
-          "neo-tree",
-          "Trouble",
-          "lazy",
-          "mason",
-          "notify",
-          "toggleterm",
-          "lazyterm",
-        },
-        callback = function()
-          ---@diagnostic disable-next-line:inject-field
-          vim.b.miniindentscope_disable = true
-        end,
-      })
-    end,
   },
 
   -- Splash screen
