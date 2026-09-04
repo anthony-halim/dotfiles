2e4e1563dc9d8457faa63c0327a64f781aff7eb1
Anthony Halim <anthonyhalim1997@gmail.com>
2026-09-04 10:42:39 +0800

    feat(nvim): add <leader>gb to open git blame mapping

--- nvim/plugin/mini_git.lua
+++ nvim/plugin/mini_git.lua
@@ -13,3 +13,24 @@ require("mini.git").setup({
 vim.keymap.set("n", "<leader>gc", function()
   require("mini.git").show_at_cursor({ split = "horizontal" })
 end, { desc = "Show at cursor", silent = true })
+
+-- Open git blame mapping (Normal Mode)
+vim.keymap.set("n", "<leader>gb", "<Cmd>vertical Git blame -- %<CR>", { desc = "Git blame", silent = true })
+
+-- Align view and bind scrolling/cursor movement for git blame
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniGitCommandSplit",
+  callback = function(ev)
+    if ev.data.git_subcommand ~= "blame" then
+      return
+    end
+    local win_src = ev.data.win_source
+    local win_blame = ev.data.win_stdout
+
+    vim.wo[win_blame].wrap = false
+    vim.fn.winrestview({ topline = vim.fn.line("w0", win_src) })
+    vim.api.nvim_win_set_cursor(win_blame, { vim.fn.line(".", win_src), 0 })
+    vim.wo[win_blame].scrollbind, vim.wo[win_src].scrollbind = true, true
+    vim.wo[win_blame].cursorbind, vim.wo[win_src].cursorbind = true, true
+  end,
+})
